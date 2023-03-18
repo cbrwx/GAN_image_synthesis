82f43723c2acf0c6598f21cd6d5adfe823ebde72
cbrwx <81207429+cbrwx@users.noreply.github.com>
2023-03-18 08:19:26 +0100

    Update main.py

--- main.py
+++ main.py
@@ -159,8 +159,8 @@ G.to(device)
 optimizerG = optim.Adam(G.parameters(), lr=lr_g, betas=(beta1, 0.999))
 
 # Add schedulers for learning rate
-schedulerD = ReduceLROnPlateau(optimizerD, mode='min', factor=0.1, patience=215, verbose=True)
-schedulerG = ReduceLROnPlateau(optimizerG, mode='min', factor=0.1, patience=215, verbose=True)
+schedulerD = ReduceLROnPlateau(optimizerD, mode='min', factor=0.02, patience=215, verbose=True)
+schedulerG = ReduceLROnPlateau(optimizerG, mode='min', factor=0.02, patience=215, verbose=True)
 
 # Load checkpoint
 if os.path.isfile(os.path.join(checkpoint_dir, 'checkpoint.pth')):
